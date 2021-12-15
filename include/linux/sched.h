bd72302f3a37e02ab01ce092d2c4fd123fcc9d54
Cyber Knight <cyberknight755@gmail.com>
2021-12-15 13:22:48 +0800

    fuse: Import `fuse_shortcircuit` from OxygenOS 11.0.0.2 drop
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1301,6 +1301,8 @@ struct task_struct {
 	 */
 	randomized_struct_fields_end
 
+	struct fuse_package *fpack;
+
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
 
@@ -1310,6 +1312,12 @@ struct task_struct {
 	 *
 	 * Do not put anything below here!
 	 */
+};
+
+struct fuse_package {
+  bool fuse_open_req;
+  struct file *filp;
+  char *iname;
 };
 
 static inline struct pid *task_pid(struct task_struct *task)
